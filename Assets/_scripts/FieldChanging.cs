c40fea52943072c41153e60b236cdeabf4f56c2a
zdoonio <dominik.zdunczyk@gmail.com>
2016-12-23 19:52:44 +0100

    complete project

---
+++ Assets/_scripts/FieldChanging.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FieldChanging : MonoBehaviour {
+
+	private char who;
+	private char pole;
+	private GameController gm;
+	public int nrPola;
+	public GameObject krzyzyk;
+	public GameObject kolko;
+
+
+	void Start()
+	{
+		GameObject managerObject = GameObject.FindGameObjectWithTag ("Game Manager");
+		if (managerObject != null)
+		{
+			gm = managerObject.GetComponent<GameController> ();
+		}
+
+	}
+
+
+	void zmianaPola(){
+
+		switch (nrPola) {
+		case 1:
+			gm.p1 = pole;
+			break;
+		case 2:
+			gm.p2 = pole;
+			break;
+		case 3:
+			gm.p3 = pole;
+			break;
+		case 4:
+			gm.p4 = pole;
+			break;
+		case 5:
+			gm.p5 = pole;
+			break;
+		case 6:
+			gm.p6 = pole;
+			break;
+		case 7:
+			gm.p7 = pole;
+			break;
+		case 8:
+			gm.p8 = pole;
+			break;
+		case 9:
+			gm.p9 = pole;
+			break;
+		default:
+			Debug.Log ("Blad panie");
+			break;
+		}
+	}
+
+	void OnMouseUp() {
+		Debug.Log ("Field Click");
+
+			if(gm.kto=='o'){
+				pole='o';
+				gm.kto='x';
+				zmianaPola ();
+				Instantiate (kolko, transform.position, transform.rotation);
+				Destroy (gameObject);
+
+			}
+			else  {
+				pole='x';
+				gm.kto='o';
+				zmianaPola ();
+				Instantiate (krzyzyk, transform.position, transform.rotation);
+				Destroy (gameObject);
+
+			}
+			gm.GetComponent<GameController>().sprawdz();
+
+
+	}
+}
+
+
---
+++ Assets/_scripts/GameController.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+
+public char p1,p2,p3,p4,p5,p6,p7,p8,p9;
+	public Text w;
+	private bool win;
+	private double czas;
+
+public char kto;
+
+
+	void Start() {
+		p1 = 'n';
+		p2 = 'n';
+		p3 = 'n';
+		p4 = 'n';
+		p5 = 'n';
+		p6 = 'n';
+		p7 = 'n';
+		p8 = 'n';
+		p9 = 'n';
+		win = false;
+	}
+
+	void Update(){
+		if (win == true) {
+			czas += Time.deltaTime;
+			Debug.Log (czas);
+			if (czas > 3.0) {
+				Application.LoadLevel (0);
+			}
+		}
+	}
+
+
+
+public void sprawdz()
+{
+		if (kto == 'x')
+			w.text = "Tura gracza: x";
+		else w.text = "Tura gracza: o";
+
+  if((p1==p2 && p2==p3 && p1!='n') ||
+  (p4==p5 && p5==p6 && p4!='n') ||
+  (p7==p8 && p8==p9 && p7!='n') ||
+  (p1==p4 && p4==p7 && p1!='n') ||
+  (p2==p5 && p5==p8 && p2!='n') ||
+  (p3==p6 && p6==p9 && p3!='n') ||
+  (p1==p5 && p5==p9 && p1!='n') ||
+  (p3==p5 && p5==p7 && p3!='n'))
+  {
+			win = true;
+			if(kto=='x') w.text="Wygrywa kółko!";
+			else  w.text="Wygrywa krzyżyk!";
+}
+	
+		if (p1 != 'n' && p2 != 'n' && p3 != 'n' &&
+		    p4 != 'n' && p5 != 'n' && p6 != 'n' &&
+		    p7 != 'n' && p8 != 'n' && p9 != 'n') {
+			win = true;
+			w.text = "REMIS";
+		}
+	}
+}
